8b6f3b96aec57770c84433f4988aa5589e4abd55
Juan Gomez <jf.gomezramiez@gmail.com>
2021-12-17 12:06:37 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl = 'https://rickandmortyapi.com/api/character';
 
-console.log('Happy hacking :)')
+const API = 'https://rickandmortyapi.com/api/character';
+
+// Construir función XHR
+// Añadir listener
+// Input
+
+const fetchData = (url_api, callback) => {
+  const xhr = new XMLHttpRequest;
+
+  xhr.open('GET',url_api, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        return callback(null, JSON.parse(xhr.responseText));
+      } else {
+        const error = new Error('Error: ' + url_api)
+        callback(error, null)
+      }
+    }
+  }
+  xhr.send();
+}
+
+fetchData(API, (error, data, ) => {
+  if (error) return console.error(error)
+  console.log(data);
+
+  
+})
+
